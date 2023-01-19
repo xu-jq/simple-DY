36cd319f1e5b33dbd0ed2a2cd0b275bd88d862f8
admin <604862834@qq.com>
2023-01-19 10:18:11 +0800

    create project

---
+++ DY-api/social-web/initialize/router.go
@@ -0,0 +1,24 @@
+package initialize
+
+import (
+	"github.com/gin-gonic/gin"
+	"simple-DY/DY-api/social-web/middlewares"
+
+	"net/http"
+)
+
+func Routers() *gin.Engine {
+	Router := gin.Default()
+	// 开启健康检查
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
+
+	//配置跨域
+	Router.Use(middlewares.Cors())
+	//添加链路追踪
+	return Router
+}
---
+++ DY-srvs/interact-srv/global/global.go
@@ -0,0 +1,10 @@
+package global
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	DB           *gorm.DB
+)
+
